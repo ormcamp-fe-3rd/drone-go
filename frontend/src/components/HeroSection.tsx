135aaa985b99dbc21c529bd768c8fe8395b83fa8
김혜빈 <hyebinkimdesign@gmail.com>
2025-01-10 20:17:15 +0900

    perf: 헤더,히어로섹션 컴포먼트 분리

---
+++ frontend/src/components/HeroSection.tsx
@@ -0,0 +1,32 @@
+export function HeroSection() {
+  return (
+    <div className="mx-auto max-w-screen-xl">
+      {/* Background Video */}
+      <video
+        className="absolute left-0 top-0 -z-10 h-full w-full object-cover"
+        autoPlay
+        muted
+        loop
+      >
+        <source src="/videos/video.mp4" type="video/mp4" />
+      </video>
+
+      {/* White Overlay */}
+      <div className="absolute left-0 top-0 z-0 h-full w-full bg-white opacity-60"></div>
+
+      {/* Content Section */}
+      <div className="relative z-10 ml-3">
+        <div className="mt-32 text-[4.17vw] font-semibold leading-[5.5vw]">
+          <p className="text-[#3D3D43]">Every Path Mapped</p>
+          <p className="text-[#3027F1]">Your Turn to Review</p>
+        </div>
+        <p className="mt-5 w-[40%] text-[14px] text-[#3D3D43]">
+          Discover the best drones with real user reviews and expert insights.
+          Our platform connects drone enthusiasts, offering detailed mobility
+          reviews, performance comparisons, and tips for every skill level.
+          Share your experiences and explore the perfect drone for your needs!
+        </p>
+      </div>
+    </div>
+  );
+}
--- frontend/src/routes/pages/HomePage.tsx
+++ frontend/src/routes/pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import { Header } from "@/components/Header";
+import { HeroSection } from "@/components/HeroSection";
 
 export function HomePage() {
   return (
     <>
       <Header />
+      <HeroSection />
     </>
   );
 }
