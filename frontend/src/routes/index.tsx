6bb2c5478a0bdc04abc036db191885921346f5f6
Devkdy22 <devkdy99@gmail.com>
2025-01-10 09:12:20 +0900

    feat:chart페이지 추가

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,15 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import App from "@/App";
+import ChartPage from "./pages/ChartPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children:[
-      // {path: "/", element: <Home />}
-  ]
-  }
-])
+    children: [
+      { path: "chart", element: <ChartPage /> },
+      // {path: "/", element: <Home />}\
+    ],
+  },
+]);
 
 export default router;
