ecc0792729ac8ea550c65232661f46359318b7ef
ban <summer.jyyy@gmail.com>
2025-01-13 14:21:40 +0900

    design: team 섹션 구현 및 이미지파일 추가

---
+++ frontend/src/components/introduce/TeamSection.tsx
@@ -0,0 +1,36 @@
+import MemberCard from "./MemberCard"
+
+export default function TeamSection(){
+  return (
+    <div className="flex w-full max-w-[1440px] flex-col items-center justify-center gap-10 pb-[700px] text-center">
+      <div className="title">Meet Our Teem</div>
+      <div className="subtitle">
+        We are a team full of warm smiles and positive energy. <br />
+        Coming from diverse backgrounds, we come together as one, supporting
+        each other and growing together.
+      </div>
+      <div className="flex gap-14">
+        <MemberCard
+          img={"/public/images/introduce/section03-01.png"}
+          name={"BanJaeYeong"}
+          roles={["TeamLeader", "Frontend"]}
+        />
+        <MemberCard
+          img={"/public/images/introduce/section03-02.png"}
+          name={"KimDoYeon"}
+          roles={["Frontend"]}
+        />
+        <MemberCard
+          img={"/public/images/introduce/section03-03.png"}
+          name={"KimSuJin"}
+          roles={["Frontend"]}
+        />
+        <MemberCard
+          img={"/public/images/introduce/section03-04.png"}
+          name={"KimHyeBin"}
+          roles={["Frontend","Designer"]}
+        />
+      </div>
+    </div>
+  );
+}
--- frontend/src/routes/pages/IntroducePage.tsx
+++ frontend/src/routes/pages/IntroducePage.tsx
@@ -1,7 +1,13 @@
-import FunctionSection from "@/components/introduce/functionSection";
+import FunctionSection from "@/components/introduce/FunctionSection";
+import TeamSection from "@/components/introduce/TeamSection";
+import UsecaseSection from "@/components/introduce/UsecaseSection";
 
 export default function IntroducePage(){
-  return(
-    <FunctionSection />
-  )
+  return (
+    <div className="flex flex-col items-center gap-40">
+      <FunctionSection />
+      <UsecaseSection />
+      <TeamSection />
+    </div>
+  );
 }
