07e694554bb4afec37321fbf1dc42f11ccd4ce7d
Devkdy22 <devkdy99@gmail.com>
2025-01-13 10:12:06 +0900

    fix:오류 사항 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
 import "./styles/index.css";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
