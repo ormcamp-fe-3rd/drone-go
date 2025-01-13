3ff75bee5a54adc7ead75d893247de9fa396ed49
ban <summer.jyyy@gmail.com>
2025-01-13 15:26:26 +0900

    design: welcome 섹션 추가, 패딩 수정

--- frontend/src/components/introduce/TeamSection.tsx
+++ frontend/src/components/introduce/TeamSection.tsx
@@ -2,7 +2,7 @@ import MemberCard from "./MemberCard"
 
 export default function TeamSection(){
   return (
-    <div className="flex w-full max-w-[1440px] flex-col items-center justify-center gap-10 pb-[700px] text-center">
+    <div className="flex w-full max-w-[1440px] flex-col items-center justify-center gap-10 pb-40 text-center">
       <div className="title">Meet Our Teem</div>
       <div className="subtitle">
         We are a team full of warm smiles and positive energy. <br />
--- frontend/src/routes/pages/IntroducePage.tsx
+++ frontend/src/routes/pages/IntroducePage.tsx
@@ -1,13 +1,15 @@
 import FunctionSection from "@/components/introduce/FunctionSection";
 import TeamSection from "@/components/introduce/TeamSection";
 import UsecaseSection from "@/components/introduce/UsecaseSection";
+import WelcomeSection from "@/components/introduce/WelcomeSection";
 
 export default function IntroducePage(){
   return (
-    <div className="flex flex-col items-center gap-40">
-      <FunctionSection />
-      <UsecaseSection />
-      <TeamSection />
-    </div>
+      <div className="flex flex-col items-center gap-40 mt-32">
+        <WelcomeSection />
+        <FunctionSection />
+        <UsecaseSection />
+        <TeamSection />
+      </div>
   );
 }
