2139d951a2155c2d5c91d5a67ad5025ebdcfeaec
ban <summer.jyyy@gmail.com>
2025-01-14 14:35:33 +0900

    design: header 컴포넌트 위치 수정, about, home 에만 적용

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,10 @@ import "./styles/input.css";
 
 import { Outlet } from "react-router-dom";
 
-import { Header } from "./components/main/Header";
 
 function App() {
   return (
     <>
-      <Header />
       <Outlet />
     </>
   );
--- frontend/src/routes/pages/HomePage.tsx
+++ frontend/src/routes/pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import { DataList } from "@/components/main/DataList";
+import { Header } from "@/components/main/Header";
 import { HeroSection } from "@/components/main/HeroSection";
 
 export function HomePage() {
   return (
     <div className="h-[2048px]">
+      <Header />
       <HeroSection />
       <DataList />
     </div>
