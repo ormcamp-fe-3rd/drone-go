a517243bf09e1bc74a1bdc6495445b91ff433eb3
김혜빈 <hyebinkimdesign@gmail.com>
2025-01-15 14:56:12 +0900

    perf: src 경로 수정

--- frontend/src/components/home/DataCard.tsx
+++ frontend/src/components/home/DataCard.tsx
@@ -14,22 +14,22 @@ export function DataCard({ day, name, timetotal }: DataCardProps) {
           <div>
             <img
               className="absolute left-9 top-10 transition-all duration-300 ease-in-out group-hover:left-4 group-hover:top-5 group-active:left-4 group-active:top-5"
-              src="../public/images/card-left-up.svg"
+              src="/images/card-left-up.svg"
               alt="카드 좌상 상세 디자인"
             />
             <img
               className="absolute right-9 top-10 transition-all duration-300 ease-in-out group-hover:right-4 group-hover:top-5 group-active:right-4 group-active:top-5"
-              src="../public/images/card-right-up.svg"
+              src="/images/card-right-up.svg"
               alt="카드 우상 상세 디자인"
             />
             <img
               className="absolute bottom-10 left-9 transition-all duration-300 ease-in-out group-hover:bottom-5 group-hover:left-4 group-active:bottom-5 group-active:left-4"
-              src="../public/images/card-left-down.svg"
+              src="/images/card-left-down.svg"
               alt="카드 좌하 상세 디자인"
             />
             <img
               className="absolute bottom-10 right-9 transition-all duration-300 ease-in-out group-hover:bottom-5 group-hover:right-4 group-active:bottom-5 group-active:right-4"
-              src="../public/images/card-right-down.svg"
+              src="/images/card-right-down.svg"
               alt="카드 우하 상세 디자인"
             />
           </div>
@@ -41,21 +41,21 @@ export function DataCard({ day, name, timetotal }: DataCardProps) {
           <div className="mx-auto my-10 flex h-24 w-36 flex-col justify-center transition-opacity duration-300 ease-in-out group-hover:hidden group-active:hidden">
             <div className="flex items-start justify-start gap-3">
               <img
-                src="../public/icons/day.svg"
+                src="/icons/day.svg"
                 className="transition-all duration-300 ease-in-out"
               />
               <p>{day}</p>
             </div>
             <div className="mt-3 flex items-start justify-start gap-3">
               <img
-                src="../public/icons/drone-sm.svg"
+                src="/icons/drone-sm.svg"
                 className="transition-all duration-300 ease-in-out"
               />
               <p>{name}</p>
             </div>
             <div className="mt-3 flex items-start justify-start gap-3">
               <img
-                src="../public/icons/clock.svg"
+                src="/icons/clock.svg"
                 className="transition-all duration-300 ease-in-out"
               />
               <p>{timetotal}</p>
