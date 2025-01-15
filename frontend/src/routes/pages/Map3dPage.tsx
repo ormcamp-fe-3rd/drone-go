788ac47cc6aa54d4598aadb69ff79ea112d691e1
ban <summer.jyyy@gmail.com>
2025-01-16 00:21:08 +0900

    feat: loading 화면 추가

--- frontend/src/routes/pages/Map3dPage.tsx
+++ frontend/src/routes/pages/Map3dPage.tsx
@@ -1,12 +1,30 @@
+import { useEffect, useState } from "react";
+
 import Map3D from "@/components/map3d/Map3D";
 import TestModel from "@/components/map3d/TestModel";
 
 
 export default function Map3dPage(){
-  return(
-    <>
-      <Map3D />
-      <TestModel />
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      setLoading(false);
+    },2000)
+  },[])
+  return (
+    <div>
+      {loading ? (
+        <div className="flex w-full justify-center items-center h-screen gap-3">
+          <img src="/icons/loading.svg" className="w-7 animate-spin" />
+          <p>Loading...</p>
+        </div>
+      ) : (
+        <div>
+          <Map3D />
+          <TestModel />
+        </div>
+      )}
+    </div>
+  );
 }
