3f5f252219dae71dbc32db63264cc337b22c3ac6
ban <summer.jyyy@gmail.com>
2025-01-15 20:13:30 +0900

    feat: 3d map 생성

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -5,31 +5,20 @@ import MapPage from "./pages/MapPage";
 import AboutPage from "./pages/AboutPage";
 import ChartPage from "./pages/ChartPage";
 import { HomePage } from "./pages/HomePage";
+import Map3dPage from "./pages/Map3dPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      // {path: "/", element: <Home />}
-      {
-        path: "/map",
-        element: <MapPage />
-      },
-
-      { path: "chart",
-        element: <ChartPage />
-      },
-
-      { path: "/",
-        element: <HomePage />
-      },
-
-      { path: "/about",
-        element: <AboutPage />
-      }
-    ]
-  }
+      { path: "/", element: <HomePage /> },
+      { path: "/about", element: <AboutPage /> },      
+      { path: "/chart", element: <ChartPage /> },
+      { path: "/map", element: <MapPage /> },
+      { path: "/map-3d", element: <Map3dPage />},
+    ],
+  },
 ]);
 
 export default router;
