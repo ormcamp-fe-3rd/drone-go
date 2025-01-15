c39c59c2ca31e2bd23c7156f2e0ec8d331527751
ban <summer.jyyy@gmail.com>
2025-01-15 20:16:03 +0900

    feat: 지도 크기 및 zoom, pitch 수정

--- frontend/src/components/map3d/Map3D.tsx
+++ frontend/src/components/map3d/Map3D.tsx
@@ -3,15 +3,18 @@ import Map from "react-map-gl";
 
 export default function Map3D() {
   return (
-    <Map
-      mapboxAccessToken={import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}
-      initialViewState={{
-        longitude: -122.4,
-        latitude: 37.8,
-        zoom: 14,
-      }}
-      style={{ width: 600, height: 400 }}
-      mapStyle="mapbox://styles/mapbox/standard"
-    />
+    <div className="fixed inset-0">
+      <Map
+        mapboxAccessToken={import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}
+        initialViewState={{
+          longitude: -122.4,
+          latitude: 37.8,
+          zoom: 17,
+          pitch: 45,
+        }}
+        style={{ width: "100%", height: "100%" }}
+        mapStyle="mapbox://styles/mapbox/standard"
+      />
+    </div>
   );
 }
