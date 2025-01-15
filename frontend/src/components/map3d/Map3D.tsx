a402fec3d5d983322cb7ccbd0aa8df56cb3a592d
ban <summer.jyyy@gmail.com>
2025-01-15 20:18:48 +0900

    feat: 지도 hooks 추가

--- frontend/src/components/map3d/Map3D.tsx
+++ frontend/src/components/map3d/Map3D.tsx
@@ -1,7 +1,20 @@
-import Map from "react-map-gl";
+import { useEffect,useRef } from "react";
+import Map, { MapRef } from "react-map-gl";
 
 
 export default function Map3D() {
+  const mapRef = useRef<MapRef>(null); //맵 인스턴스 접근
+
+  useEffect(() => {
+    const map = mapRef.current?.getMap();
+    if (!map) return;
+
+    map.on("style.load", () => {
+      // 지도 스타일이 로드된 후에 추가 작업 수행
+      //TODO: 맵 조작
+    });
+  }, []);
+  
   return (
     <div className="fixed inset-0">
       <Map
