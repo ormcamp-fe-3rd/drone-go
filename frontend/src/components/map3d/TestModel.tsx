db919a18f327557b2bedd4ca73d88c9912e81570
ban <summer.jyyy@gmail.com>
2025-01-16 16:52:17 +0900

    feat: drag rotation 적용

--- frontend/src/components/map3d/TestModel.tsx
+++ frontend/src/components/map3d/TestModel.tsx
@@ -1,19 +1,34 @@
-import { Canvas } from "@react-three/fiber";
-import { useEffect, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useEffect, useRef, useState } from "react";
 import { Mesh } from "three";
 
-const MyMesh = () => {
-  const refMesh = useRef<Mesh>(null);
+interface Props {
+  dragPosition: {
+    x: number;
+    y: number;
+  } | null;
+}
 
-  // useFrame(()=>{
-  //   if(!refMesh.current)return
-  //   refMesh.current.rotation.y += 0.01;
-  // });
+const MyMesh = ({dragPosition}:Props) => {
+  const refMesh = useRef<Mesh>(null);
+  const [rotation, setRotation] = useState<[number, number, number]>();
+
+  useEffect(()=>{
+    console.log("i'm mesh")
+    setRotation([-Math.PI/4, 0, 0])
+
+  },[])
+  useFrame(()=>{
+    if(!dragPosition)return
+    console.log("use Frame: "+dragPosition);
+    setRotation([dragPosition.x, dragPosition.y, 0])
+  });
+
   return (
     <mesh
       ref={refMesh}
       position={[0, 0, 0]}
-      rotation={[Math.PI / 4,0,0]} 
+      rotation={rotation} 
     >
       <boxGeometry args={[100, 100, 100]} />
       <meshStandardMaterial color="blue" />
@@ -21,19 +36,8 @@ const MyMesh = () => {
     </mesh>
   );
 }
-interface Props{
-  dragPosition: {
-    x: number;
-    y: number;
-  }|null;
-}
+
 export default function TestModel({dragPosition}:Props){
-  useEffect(() => {
-    if (dragPosition) {
-      console.log("Current Drag Position:", dragPosition);
-      // Perform actions based on drag position
-    }
-  }, [dragPosition]);
   
   return (
     <div className="absolute left-1/2 top-1/2 h-[200px] w-[200px] -translate-x-1/2 -translate-y-1/2">
@@ -41,7 +45,7 @@ export default function TestModel({dragPosition}:Props){
         style={{ width: "100%", height: "100%" }}
         camera={{ position: [0, 0, 200] }}
       >
-        <MyMesh />
+        <MyMesh dragPosition={dragPosition}/>
       </Canvas>
     </div>
   );
