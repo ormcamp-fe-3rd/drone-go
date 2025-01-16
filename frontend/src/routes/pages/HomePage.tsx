cb3891dc45598e7d54f70abd1b7a13d08611460c
김혜빈 <hyebinkimdesign@gmail.com>
2025-01-16 12:57:41 +0900

    fix: HomePage 로그인 모덜 삭제-> 시작 화면 로그인 모덜 비활성화로 수정

--- frontend/src/routes/pages/HomePage.tsx
+++ frontend/src/routes/pages/HomePage.tsx
@@ -1,16 +1,10 @@
 import { DataList } from "@/components/home/DataList";
 import Drone from "@/components/home/Drone";
 import { HeroSection } from "@/components/home/HeroSection";
-import { LoginModal } from "@/components/home/LoginModal";
 
 export function HomePage() {
   return (
     <div className="relative">
-      <LoginModal
-        onClose={function (): void {
-          throw new Error("Function not implemented.");
-        }}
-      />
       <Drone />
       {/* Main Content */}
       <HeroSection />
