d7686cece549b511a9be8f5e3bc853cf610ebbce
ksj0621 <r06t21@gmail.com>
2025-01-17 10:02:57 +0900

    layout:map-헤더 수정

--- frontend/src/routes/pages/MapPage.tsx
+++ frontend/src/routes/pages/MapPage.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { useLocation } from "react-router-dom";
+import { Robot, Operation } from "../../types/selectOptionsTypes";
 import { WidgetData } from "@/types/widgetDataTypes";
 import toolbarWidgetData from "../../data/toolbarWidgetData.json"
 
@@ -11,12 +13,26 @@ import ProgressBar from "@/components/map/ProgressBar";
 const MapPage: React.FC = () => {
   
   const [widgetData, _setWidgetData] = useState<WidgetData[]>(toolbarWidgetData);
+
+  const location = useLocation();
+  // 현재 URL이 "/map"인지 확인
+  const isMapPage = location.pathname === "/map";
+
+  const [selectedDrone, setSelectedDrone] = useState<Robot | null>(null);
+  const [selectedOperation, setSelectedOperation] = useState<Operation | null>(
+    null,
+  );
   
   return (
     <div className="w-full h-full m-0 bg-lime-600 box-border">
 
-      <DetailedDataHeader />
-
+      <DetailedDataHeader
+        isMapPage={isMapPage}
+        selectedDrone={selectedDrone}
+        setSelectedDrone={setSelectedDrone}
+        selectedOperation={selectedOperation}
+        setSelectedOperation={setSelectedOperation}
+      />
       <section className="toolbar grid justify-start m-4 gap-4">
         
         <ToolbarAttitude />
