58caaeb68bb37d228b76ef2bea00fbec6818d69b
ban <summer.jyyy@gmail.com>
2025-01-17 09:52:23 +0900

    fix: 중복 코드 삭제

--- backend/routes/operationRoutes.js
+++ backend/routes/operationRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const router = express.Router()
-const mongoose = require('mongoose');
 
 const Operation = require("../models/operationModel")
 const { getAllOperations, getOperationsByRobotId } = require('../controllers/operationController');
