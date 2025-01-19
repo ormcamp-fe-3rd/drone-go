4297a697a7289f738052e4fe96732c688dfcf04d
ban <summer.jyyy@gmail.com>
2025-01-19 20:15:24 +0900

    feat: header 추가

--- frontend/src/routes/pages/Map3dPage.tsx
+++ frontend/src/routes/pages/Map3dPage.tsx
@@ -1,29 +1,29 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
+import DetailedDataHeader from "@/components/charts/DetailedDataHeader";
 import Map3D from "@/components/map3d/Map3D";
+import { Operation,Robot } from "@/types/selectOptionsTypes";
 
 
 
 export default function Map3dPage(){
-  const [loading, setLoading] = useState(true);
-
-  useEffect(()=>{
-    setTimeout(()=>{
-      setLoading(false);
-    },2000)
-  },[])
+  const [selectedDrone, setSelectedDrone] = useState<Robot | null>(null);
+  const [selectedOperation, setSelectedOperation] = useState<Operation | null>(
+      null,
+  );
+  
   return (
     <div>
-      {loading ? (
-        <div className="flex h-screen w-full items-center justify-center gap-3">
-          <img src="/icons/loading.svg" className="w-7 animate-spin" />
-          <p>Loading...</p>
-        </div>
-      ) : (
-        <div>
-          <Map3D />
-        </div>
-      )}
+      <div className="fixed z-10 w-screen">
+        <DetailedDataHeader
+          isMapPage={true}
+          selectedDrone={selectedDrone}
+          setSelectedDrone={setSelectedDrone}
+          selectedOperation={selectedOperation}
+          setSelectedOperation={setSelectedOperation}
+        />
+      </div>
+      <Map3D />
     </div>
   );
 }
