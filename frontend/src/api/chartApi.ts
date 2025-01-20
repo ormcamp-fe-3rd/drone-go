6369a6e6c996013a67879aa37165ea6e8132bcd3
Devkdy22 <devkdy99@gmail.com>
2025-01-20 12:04:42 +0900

    feat:차트페이지-상태데이터(msgId=253) 필터링 하여 호출하는 코드 추가

--- frontend/src/api/chartApi.ts
+++ frontend/src/api/chartApi.ts
@@ -1,12 +1,14 @@
-import {
-  RawTelemetryBatteryData,
-  ProcessedTelemetryBatteryData,
-} from "../types/telemetryBatteryDataTypes";
+import { TelemetryData } from "../types/telemetryAllDataTypes";
+import { ProcessedTelemetryBatteryData } from "../types/telemetryBatteryDataTypes";
+import { ProcessedTelemetryTextData } from "../types/telemetryTextData";
 
 export const fetchTelemetriesByRobotAndOperation = async (
   robotId: string,
   operationId: string,
-): Promise<ProcessedTelemetryBatteryData[]> => {
+): Promise<{
+  batteryData: ProcessedTelemetryBatteryData[];
+  textData: ProcessedTelemetryTextData[];
+}> => {
   if (!robotId || !operationId) {
     throw new Error("Both robotId and operationId are required");
   }
@@ -19,10 +21,10 @@ export const fetchTelemetriesByRobotAndOperation = async (
     if (!response.ok) {
       throw new Error(`Failed to fetch telemetries: ${response.statusText}`);
     }
-    const data: RawTelemetryBatteryData[] = await response.json();
+    const data: TelemetryData[] = await response.json();
 
     // msgId가 147인 데이터만 필터링하고 필요한 값만 반환 - 배터리 온도, 전압, 잔량 데이터
-    const filterBatteryData: ProcessedTelemetryBatteryData[] = data
+    const batteryData: ProcessedTelemetryBatteryData[] = data
       .filter((telemetry) => telemetry.msgId === 147)
       .map((telemetry) => ({
         msgId: telemetry.msgId,
@@ -34,7 +36,18 @@ export const fetchTelemetriesByRobotAndOperation = async (
         },
       }));
 
-    return filterBatteryData;
+    // msgId가 253인 데이터만 필터링하고 필요한 값만 반환 - text 상태 데이터
+    const textData: ProcessedTelemetryTextData[] = data
+      .filter((telemetry) => telemetry.msgId === 253)
+      .map((telemetry) => ({
+        msgId: telemetry.msgId,
+        timestamp: new Date(telemetry.timestamp),
+        payload: {
+          text: telemetry.payload.text, // text 속성 추출
+        },
+      }));
+
+    return { batteryData, textData };
   } catch (error) {
     console.error("Error fetching telemetries:", error);
     throw error;
