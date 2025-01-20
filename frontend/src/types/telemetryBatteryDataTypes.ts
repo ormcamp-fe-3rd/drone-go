363aed94bd625d6918a6830961160076ea15cd32
Devkdy22 <devkdy99@gmail.com>
2025-01-20 12:01:59 +0900

    feat: 배터리타입 선언 수정 및 상태 텍스트 데이터 타입 추가

--- frontend/src/types/telemetryBatteryDataTypes.ts
+++ frontend/src/types/telemetryBatteryDataTypes.ts
@@ -1,14 +1,3 @@
-//데이터 받아올 때 타입
-export interface RawTelemetryBatteryData {
-  msgId: number;
-  timestamp: string;
-  payload: {
-    temperature: number;
-    batteryRemaining: number;
-    voltages: number[]; //원래 데이터에서 voltages는 배열
-  };
-}
-//가공 후 데이터 타입
 export interface ProcessedTelemetryBatteryData {
   msgId: number;
   timestamp: Date; // Date 객체로 처리
