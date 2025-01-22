14ed4e8274edea3a175b8e670997fabcd56e0269
김혜빈 <hyebinkimdesign@gmail.com>
2025-01-22 10:28:38 +0900

    pref: routes->App useScrollReset 적용

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
+import { useScrollReset } from "./hooks/useScrollReset";
 import "./styles/input.css";
 
 import { Outlet } from "react-router-dom";
 
-
 function App() {
+  useScrollReset();
   return (
     <>
       <Outlet />
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -7,26 +7,21 @@ import ChartPage from "./pages/ChartPage";
 import { HomePage } from "./pages/HomePage";
 import Map3dPage from "./pages/Map3dPage";
 import MapPage from "./pages/MapPage";
-import { ScrollToTop } from "@/hooks/scroollToTop";
-
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: (
       <>
-        {/* ScrollToTop 추가 */}
-        <ScrollToTop />
         <App />
       </>
     ),
     children: [
       { path: "/", element: <HomePage /> },
-      { path: "/about", element: <AboutPage /> },      
+      { path: "/about", element: <AboutPage /> },
       { path: "/chart", element: <ChartPage /> },
       { path: "/map", element: <MapPage /> },
-      { path: "/map-3d", element: <Map3dPage />},
-
+      { path: "/map-3d", element: <Map3dPage /> },
     ],
   },
 ]);
