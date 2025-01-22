575e063782b5e9382a815baccc8916ee14e1a78c
김혜빈 <hyebinkimdesign@gmail.com>
2025-01-22 10:27:36 +0900

    rename: git scrollToTop-> useeScrollReset

--- frontend/src/hooks/useScrollReset.tsx
+++ frontend/src/hooks/useScrollReset.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-export function useInitializeScrollOnChangePath() {
+export function useScrollReset() {
   const location = useLocation();
 
   useEffect(() => {
