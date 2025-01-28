4f75ac3aff03a4b1ac905c4ad537c261a3031fde
ban <summer.jyyy@gmail.com>
2025-01-28 21:42:38 +0900

    feat: 로그인 검증 추가(react-hook-form)

--- frontend/src/components/home/LoginForm.tsx
+++ frontend/src/components/home/LoginForm.tsx
@@ -1,15 +1,16 @@
 import { useMutation } from "@tanstack/react-query";
-import { FormEvent, useState } from "react";
+import { SubmitHandler,useForm } from "react-hook-form"
 import { useNavigate } from "react-router-dom";
 
 import { userLogin } from "@/api/userApi";
 
+interface LoginInput{
+  id: string;
+  password: string;
+}
 
 export const LoginForm = ({onSuccess}: { onSuccess: () => void;}) => {
-    const [formData, setFormData] = useState({
-      id: "",
-      password: "",
-    });
+    const { register, handleSubmit, formState: {errors} } = useForm<LoginInput>()
     const navigate = useNavigate();
     
     const loginMutation = useMutation({
@@ -23,43 +24,49 @@ export const LoginForm = ({onSuccess}: { onSuccess: () => void;}) => {
         onSuccess();
       },
       onError: (error) => {
-        console.log(error);
+        alert("Login failed. Please check your ID and password.");
+        console.error(error);
       }
     })
 
-    const handleSubmit = async (e: FormEvent) => {
-      e.preventDefault();
-      //TODO: 검증 로직 추가
-
-      loginMutation.mutate(formData);
+    const onSubmit: SubmitHandler<LoginInput> = (input) => {
+      loginMutation.mutate(input);
     };
     
   return (
-    <form onSubmit={handleSubmit} className="w-full flex flex-col items-center">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex w-full flex-col items-center"
+    >
       <input
-        type="string"
         placeholder="ID"
         className="h-[3rem] w-[30rem] rounded-[10px] border-[1px] border-[#B2B2B7] px-4 text-xl"
         autoFocus
-        value={formData.id}
-        onChange={(e) =>
-          setFormData((prev) => ({ ...prev, id: e.target.value }))
-        }
+        {...register("id", {
+          required: "ID is required",
+          minLength: {
+            value: 4,
+            message: "ID must be at least 4 characters long",
+          },
+        })}
         disabled={loginMutation.isPending}
       />
+      <div className="text-sm text-red-500 h-10 leading-8">
+        {errors.id?.message}
+      </div>
       <input
         type="password"
         placeholder="Password"
-        className="mt-7 h-[3rem] w-[30rem] rounded-[10px] border-[1px] border-[#B2B2B7] px-4 text-xl"
-        value={formData.password}
-        onChange={(e) =>
-          setFormData((prev) => ({ ...prev, password: e.target.value }))
-        }
+        className="h-[3rem] w-[30rem] rounded-[10px] border-[1px] border-[#B2B2B7] px-4 text-xl"
+        {...register("password", { required: "Password is required" })}
         disabled={loginMutation.isPending}
       />
-      <button 
+      <div className="text-sm text-red-500 h-10 leading-8 ">
+        {errors.password?.message}
+      </div>
+      <button
         type="submit"
-        className="mt-10 h-[3rem] w-[30rem] rounded-[10px] bg-black text-white"
+        className="mt-3 h-[3rem] w-[30rem] rounded-[10px] bg-black text-white"
         disabled={loginMutation.isPending}
       >
         {loginMutation.isPending ? "Signing in..." : "SIGN IN"}
