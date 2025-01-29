50f5b1aa51c4495ed4567a13d930a4a795987377
ban <summer.jyyy@gmail.com>
2025-01-29 22:11:18 +0900

    feat: 2d map 데이터 타입 변경, phase context 적용, Playhead 추가

--- frontend/src/routes/pages/MapPage.tsx
+++ frontend/src/routes/pages/MapPage.tsx
@@ -4,11 +4,16 @@ import { useState } from "react";
 import { fetchPositionDataByOperation } from "@/api/mapApi";
 import DetailedDataHeader from "@/components/charts/DetailedDataHeader";
 import Map2D from "@/components/map/Map2D";
+import AltitudeWidget from "@/components/map3d/AltitudeWidget";
+import AttitudeWidget from "@/components/map3d/AttitudeWidget";
 import MapSwitchButton from "@/components/map3d/MapSwitchButton";
-import { AttitudeWidget, BatteryState, HeadingState, SpeedAltitudeWidget, StateAlertWidget, WeatherWidget } from "@/components/map3d/Widget";
+import SpeedWidget from "@/components/map3d/SpeedWidget";
+import StateWidget from "@/components/map3d/StateWidget";
+import WeatherWidget from "@/components/map3d/WeatherWidget";
+import PhaseContextProvider from "@/contexts/PhaseContext";
 import toolbarWidgetData from "@/data/toolbarWidgetData.json"
 import { Operation,Robot } from "@/types/selectOptionsTypes";
-import formatPositionData from "@/utils/formatPositionData";
+import { formatAndSortPositionData } from "@/utils/formatPositionData";
 
 
 export default function MapPage(){
@@ -17,67 +22,58 @@ export default function MapPage(){
       null,
   );
 
-  //TODO: drone id, operation id -> 사용자가 선택한 값으로 변경 
-  const { isPending, error, data } = useQuery({
+  const { error, data } = useQuery({
     queryKey: ["position", selectedDrone, selectedOperation],
     queryFn: async () => {
+      if (!selectedDrone || !selectedOperation) return;
       const rawData = await fetchPositionDataByOperation(
-        // selectedDrone!._id,
-        // selectedOperation!._id,
-        "67773116e8f8dd840dd35155",
-        "677730f8e8f8dd840dd35153",
+        selectedDrone!._id,
+        selectedOperation!._id,
       );
-      return rawData.map(formatPositionData);
+      return formatAndSortPositionData(rawData);
     },
-    // enabled: !!selectedDrone && !!selectedOperation,
+    enabled: !!selectedOperation,
   });
-  if (isPending) return "Loading...";
   if (error) return "An error has occurred: " + error.message;
 
   return (
     <>
-      <div className="fixed z-10 w-full">
-        <DetailedDataHeader
-          backgroundOpacity={60}
-          isMapPage={true}
-          selectedDrone={selectedDrone}
-          setSelectedDrone={setSelectedDrone}
-          selectedOperation={selectedOperation}
-          setSelectedOperation={setSelectedOperation}
-        />
-      </div>
-      <div className="fixed right-10 top-[10rem] z-10">
-        <MapSwitchButton />
-      </div>
-      <div className="fixed left-4 top-[10rem] z-10">
-
-        {/* TODO: 위젯 props들 api 데이터로 수정 */}
-        <AttitudeWidget>
-          <BatteryState />
-          <HeadingState />
-        </AttitudeWidget>
-        <WeatherWidget
-          icon={toolbarWidgetData[0].icon}
-          title={toolbarWidgetData[0].title}
-          values={toolbarWidgetData[0].dataValues as string[]}
-        />
-        <SpeedAltitudeWidget
-          icon={toolbarWidgetData[1].icon}
-          title={toolbarWidgetData[1].title}
-          value={toolbarWidgetData[1].dataValues![0]}
-        />
-        <SpeedAltitudeWidget
-          icon={toolbarWidgetData[2].icon}
-          title={toolbarWidgetData[2].title}
-          value={toolbarWidgetData[2].dataValues![0]}
-        />
-        <StateAlertWidget
-          icon={toolbarWidgetData[3].icon}
-          title={toolbarWidgetData[3].title}
-          values={toolbarWidgetData[3].stateValues!}
-        />
-      </div>
-      <Map2D latLonAltData={data} />
+      <PhaseContextProvider>
+        <div className="fixed z-10 w-full">
+          <DetailedDataHeader
+            backgroundOpacity={60}
+            isMapPage={true}
+            selectedDrone={selectedDrone}
+            setSelectedDrone={setSelectedDrone}
+            selectedOperation={selectedOperation}
+            setSelectedOperation={setSelectedOperation}
+          />
+        </div>
+        <div className="fixed right-10 top-[10rem] z-10">
+          <MapSwitchButton />
+        </div>
+        <div className="fixed left-4 top-[10rem] z-10">
+          {/* TODO: 위젯 props들 api 데이터로 수정 */}
+          <AttitudeWidget/>
+          <WeatherWidget
+            icon={toolbarWidgetData[0].icon}
+            title={toolbarWidgetData[0].title}
+            values={toolbarWidgetData[0].dataValues as string[]}
+          />
+          <SpeedWidget
+            icon={toolbarWidgetData[1].icon}
+            title={toolbarWidgetData[1].title}
+            value={toolbarWidgetData[1].dataValues![0]}
+          />
+          <AltitudeWidget positionData={data ?? null} />
+          <StateWidget
+            icon={toolbarWidgetData[3].icon}
+            title={toolbarWidgetData[3].title}
+            values={toolbarWidgetData[3].stateValues!}
+          />
+        </div>
+        <Map2D positionData={data ?? null} />
+      </PhaseContextProvider>
     </>
   );
 }
