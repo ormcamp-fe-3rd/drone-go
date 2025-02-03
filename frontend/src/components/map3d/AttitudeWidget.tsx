4432582b7e89023bbb39a8bad3151ecc02656284
김혜빈 <hyebinkimdesign@gmail.com>
2025-02-03 16:59:45 +0900

    fix: AttitudeWidget 기본 설계 수정

--- frontend/src/components/map3d/AttitudeWidget.tsx
+++ frontend/src/components/map3d/AttitudeWidget.tsx
@@ -7,27 +7,31 @@ interface AttitudeWidgetProp {
 
 const AttitudeWidget = ({ heading }: AttitudeWidgetProp) => {
   return (
-    <div className="toolbar-attitude mx-6 my-0 grid hidden h-[27vh] w-[20vw] grid-cols-[1fr_1fr] grid-rows-[33%_1fr] rounded-[10px] bg-white bg-opacity-60 md:block">
-      {/* 배터리 */}
-      <div className="col-start-1 row-start-1 flex items-start justify-start p-2">
-        <div className="battery">
-          <img src="/images/battery-charging-01.svg" />
+    <div className="toolbar-attitude mx-6 my-0 hidden h-[27vh] w-[20vw] grid-cols-[1fr_1fr] grid-rows-[33%_1fr] rounded-[10px] bg-white bg-opacity-60 md:block">
+      {/* 배터리 및 헤딩을 가로로 정렬 */}
+      <div className="col-start-1 row-start-1 flex w-full items-start justify-between p-2">
+        {/* 배터리 */}
+        <div className="battery flex items-center">
+          <img
+            src="/images/battery-charging-01.svg"
+            alt="Battery"
+            className="mr-2"
+          />
+          <div className="battery-percentage text-[16px]">: 100%</div>
         </div>
-        <div className="battery-percentage text-[16px]">: 100%</div>
+
+        {/* 헤딩 */}
+        <div className="flex items-start">
+          <div className="angle mr-2 inline-block w-[35px] rounded-[30px] border-2 border-black text-center text-[12px]">
+            90°
+          </div>
+          <div>
+            <img src="/images/Frame 69.svg" alt="Heading" />
+          </div>
+        </div>
       </div>
 
-      {/* 헤딩 */}
-      <div className="col-start-2 row-start-1 flex items-start justify-end p-2">
-        <div className="angle inline-block w-[35px] rounded-[30px] border-2 border-black text-center text-[12px]">
-          90°
-        </div>
-        <div>
-          <img src="/images/Frame 69.svg" />
-        </div>
-      </div>
-      {/* 드론*/}
-
-      {/* TODO: 자세데이터 반영한 드론3d 오브젝트로 수정 */}
+      {/* 드론 3D 모델 */}
       <div className="attitude-3d relative col-span-2 row-start-2 flex items-center justify-center p-2">
         <div className="flex h-[10vh] w-[20vw] items-center justify-center">
           <Suspense fallback={<div>Loading drone...</div>}>
@@ -35,7 +39,7 @@ const AttitudeWidget = ({ heading }: AttitudeWidgetProp) => {
               scale={100}
               rotation={[0, heading, 0]}
               yAnimationHeight={0}
-              height={"20vh"}
+              height={"30vh"}
               width={"20vw"}
             />
           </Suspense>
