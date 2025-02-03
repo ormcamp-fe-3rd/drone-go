88ebe77c946b030d32e55e27f92dc2b94568860b
ban <summer.jyyy@gmail.com>
2025-02-03 22:32:49 +0900

    fix: 리뷰 반영(불필요한 state 삭제)

--- frontend/src/components/map3d/MapSwitchButton.tsx
+++ frontend/src/components/map3d/MapSwitchButton.tsx
@@ -1,18 +1,10 @@
-import { useEffect, useState } from "react";
-
 interface Props{
   is2d: boolean;
   switchMap: ()=> void;
 }
 export default function MapSwitchButton({ is2d, switchMap }: Props) {
-  const [is2dMap, setIs2dMap] = useState(false);
   const to2d = "/icons/to-2dmap.svg";
   const to3d = "/icons/to-3dmap.svg";
-
-  useEffect(()=>{
-    if(is2d === is2dMap) return
-    setIs2dMap(is2d)
-  },[is2d, is2dMap])
 
   return (
     <div className="h-16 w-16 rounded-[10px] bg-white bg-opacity-60 hover:bg-opacity-80">
@@ -20,7 +12,7 @@ export default function MapSwitchButton({ is2d, switchMap }: Props) {
         className="flex h-full w-full items-center justify-center"
         onClick={switchMap}
       >
-        <img src={is2dMap ? to3d : to2d} alt="" className="h-10 w-10" />
+        <img src={is2d ? to3d : to2d} alt="" className="h-10 w-10" />
       </button>
     </div>
   );
