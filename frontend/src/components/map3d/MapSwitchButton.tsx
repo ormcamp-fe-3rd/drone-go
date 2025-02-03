4829b9bbbcff4863d4d30910aea4e9f6e3d1add1
ban <summer.jyyy@gmail.com>
2025-02-03 14:14:19 +0900

    feat: 지도전환버튼 수정

--- frontend/src/components/map3d/MapSwitchButton.tsx
+++ frontend/src/components/map3d/MapSwitchButton.tsx
@@ -1,19 +1,27 @@
-import { Link } from "react-router-dom";
-import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-export default function MapSwitchButton(){
+interface Props{
+  is2d: boolean;
+  switchMap: ()=> void;
+}
+export default function MapSwitchButton({ is2d, switchMap }: Props) {
+  const [is2dMap, setIs2dMap] = useState(false);
   const to2d = "/icons/to-2dmap.svg";
-  const to3d = "/icons/to-3dmap.svg"
+  const to3d = "/icons/to-3dmap.svg";
 
-  const location = useLocation();
-  // 현재 URL이 "/map"인지 확인
-  const is2d = location.pathname === "/map";
-  
-  return(
-    <div className="w-16 h-16 bg-white rounded-[10px] bg-opacity-60 hover:bg-opacity-80">
-      <Link to={is2d? "/map-3d": "/map"} className="w-full h-full flex justify-center items-center">
-        <img src={is2d? to3d: to2d} alt="" className="w-10 h-10"/>
-      </Link>
+  useEffect(()=>{
+    if(is2d === is2dMap) return
+    setIs2dMap(is2d)
+  },[is2d, is2dMap])
+
+  return (
+    <div className="h-16 w-16 rounded-[10px] bg-white bg-opacity-60 hover:bg-opacity-80">
+      <button
+        className="flex h-full w-full items-center justify-center"
+        onClick={switchMap}
+      >
+        <img src={is2dMap ? to3d : to2d} alt="" className="h-10 w-10" />
+      </button>
     </div>
-  )
+  );
 }
