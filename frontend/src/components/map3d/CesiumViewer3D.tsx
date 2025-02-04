e2a61cd82e32e32f81e10be0fd5244736844b98f
ban <summer.jyyy@gmail.com>
2025-02-05 00:54:37 +0900

    fix: 3d 지도 생성 함수 분리

--- frontend/src/components/map3d/CesiumViewer3D.tsx
+++ frontend/src/components/map3d/CesiumViewer3D.tsx
@@ -1,5 +1,5 @@
 import * as Cesium from "cesium";
-import { useCallback, useContext, useEffect, useRef, useState } from "react";
+import React, { useCallback, useContext, useEffect, useRef, useState } from "react";
 
 import { PhaseContext } from "@/contexts/PhaseContext";
 import { FormattedTelemetryPositionData } from "@/types/telemetryPositionDataTypes";
@@ -43,48 +43,7 @@ const CesiumViewer: React.FC<CesiumViewerProps> = ({
   useEffect(() => {
     if (!cesiumContainerRef.current || isInitialized) return;
 
-    const initViewer = async () => {
-      try {
-        Cesium.Ion.defaultAccessToken = import.meta.env.VITE_CESIUM_ION_API_KEY;
-        const terrainProvider = await Cesium.createWorldTerrainAsync();
-
-        if (viewerRef.current) {
-          viewerRef.current.destroy();
-        }
-        if (!cesiumContainerRef.current) return;
-        viewerRef.current = new Cesium.Viewer(cesiumContainerRef.current, {
-          terrainProvider,
-          animation: false,
-          timeline: false,
-          homeButton: false,
-          sceneModePicker: false,
-          selectionIndicator: false,
-          navigationHelpButton: false,
-          fullscreenButton: false,
-          geocoder: false,
-          infoBox: false,
-          navigationInstructionsInitiallyVisible: false,
-          baseLayerPicker: false,
-          vrButton: false,
-          projectionPicker: false,
-        });
-
-        const buildingTileset = await Cesium.Cesium3DTileset.fromIonAssetId(
-          96188,
-          {
-            maximumScreenSpaceError: 16,
-          },
-        );
-
-        viewerRef.current.scene.primitives.add(buildingTileset);
-
-        setIsInitialized(true);
-      } catch (error) {
-        console.error("Failed to initialize Cesium:", error);
-      }
-    };
-
-    initViewer();
+    initViewer(viewerRef, cesiumContainerRef, setIsInitialized);
 
     return () => {
       if (viewerRef.current) {
@@ -331,3 +290,46 @@ const CesiumViewer: React.FC<CesiumViewerProps> = ({
 };
 
 export default CesiumViewer;
+
+
+const initViewer = async (
+  viewerRef: React.MutableRefObject<Cesium.Viewer | null>,
+  cesiumContainerRef: React.MutableRefObject<HTMLDivElement | null>,
+  setIsInitialized: React.Dispatch<React.SetStateAction<boolean>>,
+) => {
+  try {
+    Cesium.Ion.defaultAccessToken = import.meta.env.VITE_CESIUM_ION_API_KEY;
+    const terrainProvider = await Cesium.createWorldTerrainAsync();
+
+    if (viewerRef.current) {
+      viewerRef.current.destroy();
+    }
+    if (!cesiumContainerRef.current) return;
+    viewerRef.current = new Cesium.Viewer(cesiumContainerRef.current, {
+      terrainProvider,
+      animation: false,
+      timeline: false,
+      homeButton: false,
+      sceneModePicker: false,
+      selectionIndicator: false,
+      navigationHelpButton: false,
+      fullscreenButton: false,
+      geocoder: false,
+      infoBox: false,
+      navigationInstructionsInitiallyVisible: false,
+      baseLayerPicker: false,
+      vrButton: false,
+      projectionPicker: false,
+    });
+
+    const buildingTileset = await Cesium.Cesium3DTileset.fromIonAssetId(96188, {
+      maximumScreenSpaceError: 16,
+    });
+
+    viewerRef.current.scene.primitives.add(buildingTileset);
+
+    setIsInitialized(true);
+  } catch (error) {
+    console.error("Failed to initialize Cesium:", error);
+  }
+};
