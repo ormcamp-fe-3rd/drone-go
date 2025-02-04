a0e81ed1347efe0c29c1787169b45114c7be5860
ban <summer.jyyy@gmail.com>
2025-02-05 00:22:21 +0900

    fix: 로딩 컴포넌트 사이즈 수정

--- frontend/src/components/map3d/LoadingMessage.tsx
+++ frontend/src/components/map3d/LoadingMessage.tsx
@@ -1,6 +1,6 @@
 export default function LoadingMessage({className}: {className?:string}){
   return (
-    <div className={`fixed flex h-screen w-screen items-center justify-center ${className}`}>
+    <div className={`fixed left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 items-center justify-center ${className}`}>
       <div className="pointer-events-none flex h-20 w-56 items-center justify-center gap-5 rounded-2xl bg-white bg-opacity-90 drop-shadow-md">
         <img src="/icons/loading.svg" alt="" className="h-6 w-6 animate-spin" />
         <p className="text-center">Loading...</p>
